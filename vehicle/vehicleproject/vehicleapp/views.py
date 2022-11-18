02a69ea2fccd147041eb5e31785be2b407adcda6
jeffigeorge <jeffikalathil@gmail.com>
2022-11-18 13:36:56 +0530

    added

---
+++ vehicle/vehicleproject/vehicleapp/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import Place, Team
+
+# Create your views here.
+def home(request):
+   obj = Place.objects.all()
+   obj2 = Team.objects.all()
+   return render(request,'index.html',{'result':obj, 'teamdetails':obj2})
+
+
+
