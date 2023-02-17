83c3d06b326f9371f44ff196e5c24a54113b9c60
jeffigeorge <118290560+jeffigeorge@users.noreply.github.com>
2023-02-17 23:00:05 +0530

    Delete banking/banking directory

--- banking/banking/bankapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.index, name='register'),
-    path('login/', views.login, name='login'),
-    path('home/', views.home, name='home'),
-    path('home/',views.temp,name='home')
-]
